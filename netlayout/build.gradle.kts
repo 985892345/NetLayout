24743a0db44adefd2817a5c791c00f19628d6c8d
985892345 <guo985892345@foxmail.com>
2022-08-16 15:12:51 +0800

    :arrow_up: 升级 sdk 为 32，因为发不了依赖

--- netlayout/build.gradle.kts
+++ netlayout/build.gradle.kts
@@ -5,11 +5,11 @@ plugins {
 }
 
 android {
-  compileSdk = 31
+  compileSdk = 32
   
   defaultConfig {
     minSdk = 21
-    targetSdk = 31
+    targetSdk = 32
     
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     consumerProguardFiles("consumer-rules.pro")
@@ -38,9 +38,9 @@ configurations.all {
 }
 
 dependencies {
-  implementation("androidx.core:core-ktx:1.7.0")
-  implementation("androidx.appcompat:appcompat:1.4.1")
-  implementation("com.google.android.material:material:1.5.0")
+  implementation("androidx.core:core-ktx:1.8.0")
+  implementation("androidx.appcompat:appcompat:1.5.0")
+  implementation("com.google.android.material:material:1.6.1")
   testImplementation("junit:junit:4.13.2")
   androidTestImplementation("androidx.test.ext:junit:1.1.3")
   androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
